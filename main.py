f608cb48b0263bc1b77c220470701978aead7cc8
Eduarda Tavares <136823934+schaurert@users.noreply.github.com>
2026-08-06 22:09:43 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+cpf =  input("Digite seu CPF: ")
+telefone =  input("Digite seu telefone: ")
+anoNascimento = int (input("Digite seu ano de nascimento: "))
+
+print(f"Seu nome é: {nome}")
+print(f"Seu CPF é: {cpf}")
+print(f"Seu telefone é: {telefone}")
+print(f"Seu ano de nascimento é: {anoNascimento}")
