b2b3dfdc178b15021863ae2efa57a6c34c102644
Eduarda Tavares <eduardaschaurert@gmail.com>
2026-09-24 15:34:18 -0300

    Exercicios listas

---
+++ aula1709/exercicioLista.py
@@ -0,0 +1,63 @@
+import random
+
+# # 1. Escreva um programa que crie uma lista com números aleatórios e a imprima na tela.
+# print("\n" + "=" * 50)
+# print("EXERCÍCIO 1")
+# print("=" * 50)
+# numeros = []
+
+# for i in range(10):
+#     numero = random.randint(1, 100)
+#     numeros.append(numero)
+
+# print(numeros)
+
+# # 2. Escreva um programa que peça ao usuário três números e os armazene em uma lista. Em seguida, imprima a lista na tela.
+# print("\n" + "=" * 50)
+# print("EXERCÍCIO 2")
+# print("=" * 50)
+
+# lista = []
+
+# for i in range (3):
+#     numero = int(input(f"Digite o número da posição {i}: "))
+#     lista.append(numero)
+
+# print(f"Os números digitados foram: {lista}")
+
+# # 3. Escreva um programa que peça ao usuário uma frase e converta a frase em uma lista com cada palavra separada. Imprima a lista na tela.
+# print("\n" + "=" * 50)
+# print("EXERCÍCIO 3")
+# print("=" * 50)
+# frase = input("Digite uma frase: ")
+
+# palavras = frase.split()
+# print(palavras)
+
+# 4. Escreva um programa que crie uma lista com os números de 1 a 10 e os imprima na tela em ordem reversa.
+print("\n" + "=" * 50)
+print("EXERCÍCIO 4")
+print("=" * 50)
+numeros = []
+
+for i in range(10):
+    numero = random.randint(1, 100)
+    numeros.append(numero)
+
+print(f"Os números gerados foram: {numeros}")
+numeros.reverse()
+print(f"A ordem reversa desses números é: {numeros}")
+
+      
+# 5. Escreva um programa que crie uma lista de palavras e imprima a palavra mais longa e a palavra mais curta da lista.
+
+
+# 6. Escreva um programa que crie duas listas, uma com os números pares de 1 a 10 e outra com os números ímpares de 1 a 10. Em seguida, junte as duas listas em uma terceira lista e a imprima na tela.
+
+# 7. Escreva um programa que crie uma lista com os números de 1 a 100. Em seguida, imprima apenas os números pares da lista.
+
+# 8. Escreva um programa que crie uma lista com os números de 1 a 10 elevados ao quadrado. Em seguida, calcule a soma dos elementos da lista e imprima o resultado.
+
+# 9. Escreva um programa que crie uma lista com as letras do alfabeto e embaralhe suas posições. Em seguida, peça ao usuário para adivinhar a posição correta de uma determinada letra e informe se ele acertou ou errou.
+
+# 10. Se você terminou os exercícios acima. Tente agora fazer o jogo da velha, mas utilizando listas.
